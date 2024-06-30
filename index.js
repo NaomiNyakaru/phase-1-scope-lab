ab171b337a41707d69d8fb470b1bfa838dbd3e63
NaomiNyakaru <naomi.nyakaru@gmail.com>
2024-06-30 22:43:50 +0300

    csope-lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'Ista'
+
+function upperCaseCustomerName (){
+    window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    window.bestCustomer = 'not bob';
+
+}
+function overwriteBestCustomer(){
+    window.bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'naomi';
+}
+
+
+
